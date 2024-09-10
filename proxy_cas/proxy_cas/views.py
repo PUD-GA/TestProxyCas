ae2bdbaf2f18dd26cf19f7ddbfdebc76e459ba8c
Max Beligné <max.beligne@univ-grenoble-alpes.fr>
2024-09-10 09:49:39 +0200

    templates not working. Use HttpResponse

--- proxy_cas/proxy_cas/views.py
+++ proxy_cas/proxy_cas/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from django_cas_ng.models import ProxyGrantingTicket
 from django_cas_ng.utils import get_cas_client
 from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse
 
-def home(request):
-    return render(request, "proxy_cas/home.html")
+def home(request: HttpRequest) -> HttpResponse:
+    body = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+</head>
+<body>
+<p>Ceci est un test pour un proxy CAS</p>
+</body>
+</html>
+        """
+    return HttpResponse(body)
 
 # @csrf_exempt
 def get_pt(request):
@@ -31,7 +43,20 @@ def get_pt(request):
             if pt is not None:
                 #return JsonResponse({'pt': pt})
                 print("OK PT")
-                return render(request, "proxy_cas/testok.html")
+
+                body = """<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Title</title>
+                        </head>
+                        <body>
+                        ça à l'air de marcher
+                        </body>
+                        </html>
+                        """
+                return HttpResponse(body)
+                #return render(request, "proxy_cas/testok.html")
             else:
                 response = JsonResponse({'status':'false','message': 'Invalid pgtiou'})
                 response.status_code = 400
